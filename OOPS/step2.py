6c610f556a10cea6f12074ac22b2b4379a448f1c
Prasannnnn <prasannamb4362@gmail.com>
2024-06-07 11:02:42 +0530

    add the code of step2

---
+++ OOPS/step2.py
@@ -0,0 +1,41 @@
+'''
+class Instance methods and class methods
+'''
+
+class Student:
+    
+    school = "St.paul"
+
+    def __init__(self,m1,m2,m3) -> None:
+        self.maths=m1
+        self.science=m2
+        self.social=m3
+
+    def avg(self):
+        return (self.maths+self.science+self.social)/3
+    
+    def get(self):
+        return self.maths
+    
+    def set(self):
+        self.maths = 80
+
+    @classmethod
+    def info(cls):
+        return cls.school
+
+stu = Student(98,78,89)
+mah = Student(89,54,64)
+
+
+'''
+Accessor only fetch the variables
+Mutators only modify the variables
+'''
+print(stu.avg())
+print(mah.avg())
+
+print(stu.set())
+print(stu.maths)
+
+print(Student.info())
