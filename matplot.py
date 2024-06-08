f4e6d9a6e76b45d85961511127ed8f8af62dfcb8
Prasannnnn <prasannamb4362@gmail.com>
2024-06-08 10:32:54 +0530

    matplot.py

--- matplot.py
+++ matplot.py
@@ -100,15 +100,15 @@ now you will get the output but not as markers , so we have to mark it
 --->marker = '*'
 
 '''
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-import numpy as np
+# import numpy as np
 
-a = np.array([5,8,2,9,6])
+# a = np.array([5,8,2,9,6])
 
-plt.plot(a,marker='*')
+# plt.plot(a,marker='*')
 
-plt.show()
+# plt.show()
 
 '''
 Marker Reference
@@ -141,3 +141,289 @@ Marker	Description
 '_'	--> Hline
 
 '''
+
+
+'''
+Line plot
+'''
+################################################08/06/24
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 8, 1, 10])
+
+# # plt.plot(ypoints, linestyle = 'dotted') # it is for dotted
+# # plt.show()
+
+# plt.plot(ypoints, linestyle = 'dashed') # it is for dashed
+# plt.show()
+
+'''
+Shorter Syntax
+The line style can be written in a shorter syntax:
+
+linestyle can be written as ls.
+
+dotted can be written as :.
+
+dashed can be written as --.
+'''
+
+'''
+Line Styles
+You can choose any of these styles:
+
+Style	Or
+'solid' (default) --->	'-'	
+'dotted'	      ===>  ':'	
+'dashed'	      --->  '--'	
+'dashdot'	      --->  '-.'	
+'None'	          ===>  '' or ' '	
+'''
+'''
+Line Color
+
+You can use the keyword argument color or the shorter c to set the color of the line:
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+# plt.plot(ypoints, color = 'r')
+# plt.show()
+
+'''
+You can also use Hexadecimal color values:
+'''
+# plt.plot(ypoints, c = '#4CAF50')
+# plt.show()
+
+
+'''
+Plot with the color named "hotpink":
+'''
+
+# plt.plot(ypoints, c = 'hotpink')
+# plt.show()
+
+'''
+Line Width
+You can use the keyword argument linewidth or the shorter lw to change the width of the line.
+
+The value is a floating number, in points:
+'''
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 8, 1, 10])
+
+# plt.plot(ypoints, linewidth = '20.5')
+# plt.show()
+
+'''
+Multiple Lines
+You can plot as many lines as you like by simply adding more plt.plot() functions:
+
+'''
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y1 = np.array([3, 8, 1, 10])
+# y2 = np.array([6, 2, 7, 11])
+
+# plt.plot(y1)
+# plt.plot(y2)
+
+# plt.show()
+
+'''
+You can also plot many lines by adding the points for the x- and y-axis for each line in the same plt.plot() function.
+
+(In the examples above we only specified the points on the y-axis, meaning that the points on the x-axis got the the default values (0, 1, 2, 3).)
+
+The x- and y- values come in pairs:
+'''
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x1 = np.array([0, 1, 2, 3])
+# y1 = np.array([3, 8, 1, 10])
+# x2 = np.array([0, 1, 2, 3])
+# y2 = np.array([6, 2, 7, 11])
+
+# plt.plot(x1, y1, x2, y2)
+# plt.show()
+
+
+'''
+Matplotlib label and title
+'''
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.plot(x, y)
+
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+
+# plt.show()
+
+
+'''
+Create a Title for a Plot
+With Pyplot, you can use the title() function to set a title for the plot.
+'''
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.plot(x, y)
+
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+
+# plt.show()
+
+
+'''
+Set Font Properties for Title and Labels
+You can use the fontdict parameter in xlabel(), ylabel(), and title() 
+to set font properties for the title and labels.
+'''
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# font1 = {'family':'serif','color':'blue','size':20}
+# font2 = {'family':'serif','color':'darkred','size':15}
+
+# plt.title("Sports Watch Data", fontdict = font1)
+# plt.xlabel("Average Pulse", fontdict = font2)
+# plt.ylabel("Calorie Burnage", fontdict = font2)
+
+# plt.plot(x, y)
+# plt.show()
+
+
+'''
+Position the Title
+You can use the loc parameter in title() to position the title.
+
+Legal values are: 'left', 'right', and 'center'. Default value is 'center'.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data", loc = 'left')
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+plt.show()
+
+
+'''
+Matplotlib Adding Grid Lines
+
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid()
+
+plt.show()
+
+
+
+'''
+Specify Which Grid Lines to Display
+You can use the axis parameter in the grid() function to specify which grid lines to display.
+
+Legal values are: 'x', 'y', and 'both'. Default value is 'both'.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(axis = 'x')
+
+plt.show()
+
+
+
+'''
+Display only grid lines for the x-axis:
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(axis = 'x')
+
+plt.show()
+
+'''
+Display only grid lines for the y-axis:
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(axis = 'y')
+
+plt.show()
+
+
