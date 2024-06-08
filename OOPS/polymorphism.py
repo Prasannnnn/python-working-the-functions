1f2735af3e51fce228399ec73b649074b9e7b236
Prasannnnn <prasannamb4362@gmail.com>
2024-06-08 11:31:26 +0530

    updated matplot.py

---
+++ OOPS/polymorphism.py
@@ -0,0 +1,15 @@
+'''
+Objects have multiple forms
+---> Duck typing
+---> Method Overloading
+---> Method Overriding
+'''
+
+'''
+Duck Typing
+One thing in Many forms
+Which is walking like a duck
+which is quack like a duck
+which
+
+'''
