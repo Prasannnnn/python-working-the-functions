ab8d70badeb5914b27154e27efbbab5b162fa151
Prasannnnn <prasannamb4362@gmail.com>
2024-06-10 12:24:18 +0530

    added polymorphism

--- OOPS/method_overriding.py
+++ OOPS/method_overriding.py
@@ -1,3 +1,11 @@
+'''
+Method overriding is an ability of any object-oriented programming language that allows a 
+subclass or child class to provide a specific implementation of a method that is already 4
+provided by one of its super-classes or parent classes. When a method in a subclass has the same name, 
+same parameters or signature and same return type(or sub-type) as a method in its super-class, 
+then the method in the subclass is said to override the method in the super-class.
+'''
+
 class Animal():
     def sound(self):
         print("Animal makes sound")
