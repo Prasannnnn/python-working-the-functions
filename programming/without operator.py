c317a043e68109dbf901b7f4beaec29731efeec5
Prasannnnn <prasannamb4362@gmail.com>
2024-06-21 13:07:26 +0530

    add two files

---
+++ programming/without operator.py
@@ -0,0 +1,36 @@
+def add_without_plus(a,b):
+    while b!=0:
+        c = a & b
+        a = a ^ b
+        b = c << 1
+    return a
+
+num1 = 5
+num2 = 7
+result = add_without_plus(num1,num2)
+print(result)
+
+
+"""
+The << operator inserts the specified number of 0's (in this case 2) from the right and let the same amount of leftmost bits fall off:
+
+If you push 00 in from the left:
+ 3 = 0000000000000011
+becomes
+12 = 0000000000001100
+
+Decimal numbers and their binary values:
+ 0 = 0000000000000000
+ 1 = 0000000000000001
+ 2 = 0000000000000010
+ 3 = 0000000000000011
+ 4 = 0000000000000100
+ 5 = 0000000000000101
+ 6 = 0000000000000110
+ 7 = 0000000000000111
+ 8 = 0000000000001000
+ 9 = 0000000000001001
+10 = 0000000000001010
+11 = 0000000000001011
+12 = 0000000000001100
+"""
