21c4fc016ca97a1717ba4f82f2a248edc4b4d59a
Prasannnnn <prasannamb4362@gmail.com>
2024-06-22 15:07:58 +0530

    changes the things

---
+++ Regular-Expression/match.py
@@ -0,0 +1,8 @@
+'''
+A Match Object is an object containing information about the search and the result.
+Note: If there is no match, the value None will be returned, instead of the Match Object.
+Do a search that will return a Match Object:
+'''
+import re
+txt = "I have ASD and autism patient"
+
